377299d2034c075c99d79fda416660847afc87ce
hugo-rgr <hugo@silk-digital.com>
2025-04-04 17:02:46 +0200

    init calculator

---
+++ TP1_BDD_Calculator/Drivers/Driver.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace TP1_BDD_Calculator.Drivers
+{
+    public class Driver
+    {
+    }
+}
---
+++ TP1_BDD_Calculator/Hooks/Hook.cs
@@ -0,0 +1,10 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace TP1_BDD_Calculator.Hooks
+{
+    [Binding]
+    public class Hooks
+    {
+    }
+}
